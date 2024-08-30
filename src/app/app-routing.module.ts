345f33b20ad1888c4b5fa3ff4a005fbc48fb4129
BreMiranda <brendatw43@gmail.com>
2024-08-30 20:10:29 -0300

    commit inicial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { PostListComponent } from './post-list/post-list.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent },
+  {path: 'about', component: AboutComponent },
+  {path: 'postList', component: PostListComponent },
+  {path: 'postDetail/:id', component: PostDetailComponent },
+  {path: '**', component: NotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
